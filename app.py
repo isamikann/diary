7f63be95a14ca5a5ca2093212ed7c2ae8a4fa62c
isamikann <isamu.mizoguchi.1994@gmail.com>
2025-03-15 00:07:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -940,7 +940,7 @@ def main():
     # サイドバーメニュー
     menu = st.sidebar.radio(
         "メニュー",
-        ["✍️ 日記を書く", "📅 過去の日記", "📊 データ分析", "⚙️ 設定・ヘルプ"],
+        ["✍️ 日記を書く", "📅 過去の日記", "📊 データ分析", "📊高度な可視化分析",　"⚙️ 設定・ヘルプ"],
     )
     
     if menu == "✍️ 日記を書く":
@@ -952,7 +952,7 @@ def main():
     elif menu == "📊 データ分析":
         show_statistics()
       
-    elif menu == "📊 高度な可視化分析":
+    elif menu == "📊高度な可視化分析":
         advanced_visualizations(df)
     
     elif menu == "⚙️ 設定・ヘルプ":
